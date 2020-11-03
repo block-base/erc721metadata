3d29bf0fbe2c3fe6abbad16824f317f47d8a28b2
ookimaki <noreply@ookimaki.com>
2020-11-03 10:01:09 +0900

    modify before deploy

--- scripts/tataminin/build.ts
+++ scripts/tataminin/build.ts
@@ -10,7 +10,7 @@ interface Metadata {
 }
 
 const count = 100;
-const OUTPUT_PATH_BASE = "./public/tataminin/";
+const OUTPUT_PATH_BASE = "./public/api/tataminin/";
 
 for (let i = 1; i <= count; i++) {
   const metadata: Metadata = {
